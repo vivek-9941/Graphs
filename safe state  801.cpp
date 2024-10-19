1714ef68b4f39e0172d47812726a461df186d37c
vivek-9941 <vivekmane9860.com>
2024-10-19 09:25:06 +0530

    leetcode 801 safer states

---
+++ safe state  801.cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>> &graph)
+    {
+        // reversal;
+        int n = graph.size();
+
+        vector<int> indegree(n, 0);
+        vector<vector<int>> adjrev(n);
+        for (int i = 0; i < n; i++)
+        {
+            for (auto it : graph[i])
+            {
+                adjrev[it].push_back(i);
+                indegree[i]++;
+            }
+        }
+
+        queue<int> q;
+        for (int i = 0; i < n; i++)
+        {
+            if (indegree[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+        vector<int> safenode;
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            safenode.push_back(node);
+
+            for (auto it : adjrev[node])
+            {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.push(it);
+            }
+        }
+        sort(safenode.begin(), safenode.end());
+        return safenode;
+    }
+};
